ca97c05dfbcad692f285fa4bbea7aa8fd950b07a
Duggu Jhansi <119596105+Duggu-Jhansi@users.noreply.github.com>
2024-03-05 14:49:15 +0530

    Update MovieDetail.vue

--- components/MovieDetail.vue
+++ components/MovieDetail.vue
@@ -14,7 +14,7 @@ defineProps(["title", "date", "poster", "revenue", "overview", "trailer"]);
     </div>
 
     <!-- Column 2: Title, Overview, revenue, Buy Button -->
-    <div class="sm:col-span-2 mx-6 shadow-2xl">
+    <div class="sm:col-span-2 mx-6 shadow-2xl p-2">
       <h1 class="font-bold text-2xl mb-5">{{ title }}</h1>
       <h3 class="font-semibold text-lg mb-5">Release Date: {{ date }}</h3>
       <p class="text-gray-500 text-md mb-8">
@@ -31,11 +31,7 @@ defineProps(["title", "date", "poster", "revenue", "overview", "trailer"]);
         >
           Watch Trailer
         </NuxtLink>
-        <button
-          class="bg-rose-500 hover:bg-rose-600 text-white font-medium py-2 px-4 rounded m-5"
-        >
-          Buy Now
-        </button>
+        
       </div>
     </div>
   </div>
